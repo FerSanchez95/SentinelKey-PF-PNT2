cc8fd4e28e223a5d3d99f3bf8195a9c1fed9f355
FerSanchez95 <fersanchezz95@gmail.com>
2025-06-16 22:03:30 -0300

    Añade comentarios descriptivos en el componente Password para mejorar la claridad sobre la funcionalidad esperada.

--- src/features/password/pages/Password.jsx
+++ src/features/password/pages/Password.jsx
@@ -3,6 +3,17 @@ import Card from '../../../components/Card/Card.jsx'
 
 export default function Password() {
 
+    /**
+     * ¿Tiene que trarer todas las contraseñas?
+     * - Modificar las tarjetas par que queden más presentables.
+     * - Cada elemento 'Card' tiene que poder ser seleccionado.
+     * - Las tarjetas deben poder selecionarse y editarse individualmente.
+     * - Las contraseñas, como entidad, tienen que poder ser:
+     *      - Editadas (individualmente)
+     *      - Eliminadas (individualmente o varias en una sola consulta)
+     * - Las consultas deben ser hechas a partir del 'id' o el 'usuario_id' (propiedades de la tabla en SB).
+     * - Agrupoar la lógica de negocio en el servicio. 
+    */
     return (
         <Card titulo="Gmail" />
     );
