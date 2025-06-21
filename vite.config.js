0d19e344216e1ab676d820426a589beac8ff37f8
FerSanchez95 <fersanchezz95@gmail.com>
2025-06-21 09:35:16 -0300

    [Feat] agrega iconos para PWA en la configuración de Vite

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,6 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 const manifestForPlugin = {
   registerType: 'autoUpdate',
-  includeAssets: ['react.svg', 'apple-touch-icon.png'],
   strategies: 'generateSW',
   manifest: {
     name: 'SentinelKey - Gestor de contraseñas',
@@ -15,27 +14,64 @@ const manifestForPlugin = {
     background_color: '#1a1a1a',
     start_url: '/',
     scope: '/',
-    display_override: ["minimal-ui"],
-    display: 'minimal-ui',
+    display: 'standalone',
     orientation: 'portrait',
-    protocol_handlers: [
-      {
-      "protocol": "web+https",
-      "url": "/https?type=%s"
+    icons: [
+    {
+      "src": "icons/icon-48x48.png",
+      "sizes": "48x48",
+      "type": "image/png"
     },
     {
-      "protocol": "web+coffee",
-      "url": "/coffee?type=%s"
-    }],
-    icons: [
-          {
-            src: 'react.svg',
-            sizes: '192x192',
-            type: 'image/png'
-          }
-        ]
+      "src": "icons/icon-72x72.png",
+      "sizes": "72x72",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-96x96.png",
+      "sizes": "96x96",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-128x128.png",
+      "sizes": "128x128",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-144x144.png",
+      "sizes": "144x144",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-152x152.png",
+      "sizes": "152x152",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-256x256.png",
+      "sizes": "256x256",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-384x384.png",
+      "sizes": "384x384",
+      "type": "image/png"
+    },
+    {
+      "src": "icons/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+    ],
   },
   workbox: {
+
+    navigateFallback: 'index.html',
     globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
     runtimeCaching: [
       {
@@ -65,7 +101,6 @@ export default defineConfig({
     VitePWA(manifestForPlugin)],
     server: {
       host: true,
-      https: false
     },
     build: {
       sourcemap: true
